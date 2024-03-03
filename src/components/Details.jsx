0e44957537b588cb3e7084b3822c0b31d6fbe30b
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-03-04 00:32:06 +0530

    adding another componenet

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,18 +1,39 @@
 import React,{useState} from 'react'
 import Form from './Form'
+import ListOfDetails from './ListOfDetails'
 
 const Details = () => {
 
-    let [details,setDetails] = useState([]);
+    // let [details,setDetails] = useState([]);
+    // const GetDataFromFormHandler = (dataFromForm)=>{
+    //     setDetails((orevState)=>[...orevState,dataFromForm]);
+    // }
+
+
+    const [details,setDetails] = useState([
+        {name:'ALok', age:25},
+        {name:"amit",age:30}
+    ])
+
     const GetDataFromFormHandler = (dataFromForm)=>{
-        setDetails((orevState)=>[...orevState,dataFromForm]);
+        let includesIdInDetails = {
+            ...dataFromForm,
+            id:Math.random().toString()
+
+        }
+        setDetails((prevState)=>[...prevState,includesIdInDetails]);
+        console.log(includesIdInDetails)
     }
 
 
   return (
     <div>
         <Form onGetDataFromForm={GetDataFromFormHandler}/>
-      <h1>details</h1>
+         {
+         details.map((itm)=>(
+            <ListOfDetails id={itm.id} name={itm.name} age={itm.age}/>
+                             )
+          )}
     </div>
   )
 }
